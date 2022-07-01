82d1b70814a6824a7d09ecb9f223ab04c8fd9ffa
Abduvoris <abikthebest01@gmail.com>
2022-07-01 19:40:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//  Git is a version control system that lets you manage and keep track of your source code history.
+
+// GitHub is a cloud-based hosting service that lets you manage Git repositories.
+
+// git init
+
+// Configuring Git
+// A lot of people can use git so we have to configure it with our email and name
+
+// git config
+// You can configure locally and globally
+// git config --local user.name "Ivan"
+// git config --local user.email ivan@gmail.com
+
+// You can see changes in git/.config
+// You have to set global
+
+// git config --global user.email ivan@gmail.com
+
+// 3 stages of files
+// 1. when files just created
+// 2. When git looking for some files
+// 3. When git is created some control point the place where you can return and see how project worked === commit
+
+
+// Check repositry status
+// git status
+// git add -A
+
+// create main.css
+// git add main.css
+
+// git commit -a -m"first commit"
